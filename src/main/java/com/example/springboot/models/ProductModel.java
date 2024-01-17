ce23bb3eae388b6992b593558128945a48306873
Pablo Aquino <pabloaquino1988@gmail.com>
2024-01-17 10:19:53 -0300

    Dockerfile added

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,25 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+}
